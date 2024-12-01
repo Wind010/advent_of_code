8038e67d348158f39c7211914072d27660c3e03f
wind010 <wind010@users.noreply.github.com>
2024-12-01 01:36:23 -0800

    Day 1 of 2024

---
+++ 2024/1/part2.py
@@ -0,0 +1,65 @@
+'''
+--- Part Two ---
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+For these example lists, here is the process of finding the similarity score:
+
+The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+The fourth number, 1, also does not appear in the right list.
+The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
+'''
+
+
+
+import re
+from functools import reduce
+from common.common import arg_parse, timer
+from collections import Counter, defaultdict
+
+
+
+def main(file_path):
+    data = open(file_path, 'r', encoding='utf-8').read()
+
+    lines = data.strip().split('\n')
+    
+    col1 = [int(line.split()[0]) for line in lines]
+    col2 = [int(line.split()[1]) for line in lines]
+    
+    score = sum([n * col2.count(n)for n in col1])
+
+    print(score)
+    if 'input1.txt' in file_path: assert score == 31
+    if 'input2.txt' in file_path: assert score == 24643097
+
+
+    return score
+    
+
+
+if __name__ == "__main__":
+    args = arg_parse(__file__, 'input1.txt', main)
+    main(args.file_path)
+    args = arg_parse(__file__, 'input2.txt', main)
+    main(args.file_path)
+
