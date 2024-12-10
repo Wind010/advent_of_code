f5bb1bcee3254a9d0cf21194bdd6dc051712c5b1
wind010 <wind010@users.noreply.github.com>
2024-12-10 00:32:52 -0800

    Day 10 of 2024 adding BFS/DFS to part1 and 2

--- 2024/10/part1.py
+++ 2024/10/part1.py
@@ -5,9 +5,10 @@ https://adventofcode.com/2024/day/10
 
 from collections import defaultdict
 from common.common import arg_parse, assertions, timer
-
+from collections import deque
 
 DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 
 def find_trail_heads(grid):
     ''' Time complexity:  O(n*m)'''
@@ -24,32 +25,81 @@ def is_in_bounds(x, y, grid):
     return 0 <= x < len(grid) and 0 <= y < len(grid[0])
 
 
-def dfs(r, c, grid, visited, target, path, paths):
-    if grid[r][c] == target:
-        path.append((r, c))
-        paths.append(list(path))
+def dfs_stack(start_row, start_col, grid):
+    stack = [(start_row, start_col)]
+    visited = set()
+    path = []
+
+    while stack:
+        r, c = stack.pop()
+        if (r, c) in visited:
+            continue
+        
+        visited.add((r, c))
+
+        if grid[r][c] == 9:
+            path.append((r, c))
+
+        for d_r, d_c in DIRECTIONS:
+            n_r, n_c = r + d_r, c + d_c
+            
+            if is_in_bounds(n_r, n_c, grid) and grid[n_r][n_c] == grid[r][c] + 1:
+                stack.append((n_r, n_c))
+    
+    return path
+
+
+
+def dfs_recursive(row, col, grid, visited, path):
+    if (row, col) in visited or not is_in_bounds(row, col, grid):
         return
+
+    visited.add((row, col))
     
-    visited[r][c] = True
-    path.append((r, c))
+    if grid[row][col] == 9:
+        path.append((row, col))
     
-    for d_r, dy_c in DIRECTIONS:
-        n_r, n_c = r + d_r, c + dy_c
-        # Check if the new cell is within bounds, not visited, and has a value greater than the current cell by 1
-        if is_in_bounds(n_r, n_c, grid) and grid[n_r][n_c] == grid[r][c] + 1:
-            dfs(n_r, n_c, grid, visited, 9, path, paths)
+    for d_r, d_c in DIRECTIONS:
+        n_r, n_c = row + d_r, col + d_c
+        
+        if is_in_bounds(n_r, n_c, grid) and grid[n_r][n_c] == grid[row][col] + 1:
+            dfs_recursive(n_r, n_c, grid, visited, path)
+
+
+@timer
+def find_trailhead_paths_dfs_recursive(grid):
+    trail_heads = find_trail_heads(grid)
+    all_paths = {}
     
-    visited[r][c] = False
-    path.pop()
+    for start_x, start_y in trail_heads:
+        visited = set()
+        path = []
+        dfs_recursive(start_x, start_y, grid, visited, path)
+        all_paths[(start_x, start_y)] = path
+    
+    return all_paths
+
+
+@timer
+def find_trailhead_paths_dfs_stack(grid):
+    trail_heads = find_trail_heads(grid)
+    all_paths = {}
+
+    for start_x, start_y in trail_heads:
+        path = dfs_stack(start_x, start_y, grid)
+        all_paths[(start_x, start_y)] = path
+
+    return all_paths
 
 
 def bfs(start_row, start_col, grid):
-    queue = [(start_row, start_col)]
+    # Use deque since can append elements at the end in O(1), but removing elements from the 
+    # begining requires shifting all remaining elements to the left, which is O(n).
+    queue = deque([(start_row, start_col)])
     visited, path = set(), []
     
     while queue:
-        # Get the first element from the queue
-        r, c = queue.pop(0)  # Use pop(0) for FIFO
+        r, c = queue.pop()
         
         if (r, c) in visited:
             continue
@@ -58,8 +108,7 @@ def bfs(start_row, start_col, grid):
         
         if grid[r][c] == 9:
             path.append((r, c))
-        
-        # Checkout all valid neighboring cells
+
         for d_r, d_c in DIRECTIONS:
             n_r = r + d_r
             n_c = c + d_c
@@ -73,6 +122,10 @@ def bfs(start_row, start_col, grid):
 
 @timer
 def find_trailhead_paths_bfs(grid):
+    '''
+    Time complexity:  O(n*m).
+    Space complexity: O(n*m)
+    '''
     rows, cols = len(grid), len(grid[0])
     trail_heads = find_trail_heads(grid)
     all_paths = {}
@@ -95,18 +148,27 @@ def find_trailhead_paths_bfs(grid):
 def main(args, data):
     num_grid = [[int(d) for d in line] for line in data.strip().split('\n')]
     
-    paths = find_trailhead_paths_bfs(num_grid)
-
-    scores = [len(p) for p in paths.values()]
-    total_score = sum(scores)
+    paths1 = find_trailhead_paths_bfs(num_grid)
+    scores1 = [len(p) for p in paths1.values()]
+    total_score1 = sum(scores1)
     
-    assertions(args, total_score, 36, 517)
+    paths2 = find_trailhead_paths_dfs_stack(num_grid)
+    scores2 = [len(p) for p in paths2.values()]
+    total_score2 = sum(scores2)
+    
+    paths3 = find_trailhead_paths_dfs_recursive(num_grid)
+    scores3 = [len(p) for p in paths3.values()]
+    total_score3 = sum(scores3)
+    
+    assert total_score1 == total_score2 == total_score3
+    
+    assertions(args, total_score1, 36, 517)
 
-    return total_score
+    return total_score1
     
 
 
 if __name__ == "__main__":
-    args = arg_parse(__file__, 'input3.txt', main)
+    args = arg_parse(__file__, 'input1.txt', main)
     args = arg_parse(__file__, 'input2.txt', main)
 
--- 2024/10/part2.py
+++ 2024/10/part2.py
@@ -6,7 +6,33 @@ https://adventofcode.com/2024/day/10
 import re
 from common.common import arg_parse, assertions, timer
 from part1 import find_trail_heads, DIRECTIONS, is_in_bounds
+from collections import deque
 
+
+def bfs(start_row, start_col, grid):
+    queue = deque([(start_row, start_col)])
+    visited, paths = set(), []
+    
+    while queue:
+        row, col = queue.popleft()
+
+        if grid[row][col] == 9:
+            paths.append((row, col))
+            continue
+
+        if (row, col) in visited:
+            continue
+
+        visited.add((row, col))
+
+        for d_row, d_col in DIRECTIONS:
+            n_r = row + d_row
+            n_c = col + d_col
+
+            if is_in_bounds(n_r, n_c, grid) and grid[n_r][n_c] == grid[row][col] + 1:
+                queue.append((n_r, n_c))
+
+    return paths
 
  
 def dfs(row, col, grid, visited):    
@@ -28,8 +54,29 @@ def dfs(row, col, grid, visited):
     
     return paths
     
+    
+@timer
+def find_trailhead_paths_bfs(grid):
+    '''
+    Time complexity:  O(n*m).
+    Space complexity: O(n*m)
+    '''
+    trail_heads = find_trail_heads(grid)
+    all_paths = {}
 
+    for start_x, start_y in trail_heads:
+        path = bfs(start_x, start_y, grid)
+        all_paths[(start_x, start_y)] = path
+    
+    return all_paths
+
+
+@timer
 def find_trailhead_paths_dfs(grid):
+    '''
+    Time complexity:  O(n*m).
+    Space complexity: O(n*m)
+    '''
     trail_heads = find_trail_heads(grid)
     all_paths, visited = {}, {}
 
@@ -44,13 +91,18 @@ def main(args, data):
     num_grid = [[int(d) for d in line] for line in data.strip().split('\n')]
     
     paths = find_trailhead_paths_dfs(num_grid)
-
     scores = [len(p) for p in paths.values()]
-    total_score = sum(scores)
+    total_score1 = sum(scores)
     
-    assertions(args, total_score, 81, 1116)
+    paths2 = find_trailhead_paths_dfs(num_grid)
+    scores2 = [len(p) for p in paths2.values()]
+    total_score2 = sum(scores2)
+    
+    assert total_score1 == total_score2
+    
+    assertions(args, total_score1, 81, 1116)
 
-    return total_score
+    return total_score1
     
 
 
