fe9df9625900cfbf7fd3eafac3317511606027ed
wind010 <wind010@users.noreply.github.com>
2024-12-09 18:51:23 -0800

    Update part 1 of day 8 of 2024

--- 2024/9/part1.py
+++ 2024/9/part1.py
@@ -12,18 +12,20 @@ DOT = '.'
 
 @timer
 def defrag_original(data):
+    ''' Time complexity of O(f*fs + k + cb) where k is free space not defragmented and cb are actual contiguous blocks.'''
     #disk_map = dict(zip(data[::2], data[1::2]))
-    # blocks, file_id = [], 0
-    # for files, free_space in list(zip(data[::2], data[1::2])):
-    #     blocks.extend(str(file_id) * int(files) + DOT * int(free_space))
-    #     file_id += 1
-    
+    blocks, file_id = [], 0
+    for i in range(0, len(data) - 1, 2):
+        files = data[i]
+        free_space = data[i + 1]
+        blocks.extend([str(file_id)] * int(files))
+        blocks.extend([DOT] * int(free_space))
+        file_id += 1
 
-    # if len(data) % 2 != 0:
-    #     blocks += str(file_id) * int(data[-1])
-    #     file_id += 1
-
-    blocks = [i//2 if i%2 == 0 else DOT for i, num in enumerate(data) for _ in range(int(num))]
+    # Handle the case where there is an unpaired file size at the end
+    if len(data) % 2 != 0:
+        blocks.extend([str(file_id)] * int(data[-1]))
+        file_id += 1
 
 
     i, j = 0, len(blocks) - 1
@@ -58,36 +60,55 @@ def defrag_original(data):
 
 
 @timer
-def defrag_clean(data):
+def defrag_cleaner_but_slower(data):
+    ''' 
+    Time complexity of O(f*fs + k*b + cb) where k is free space not defragmented and cb are actual contiguous blocks.
+    Where b is the iteration through blocks looking for the next index of DOT.
+    '''
     # Populate as original, but using file ID as every even number starting wtih 0.
     blocks = [i//2 if i%2 == 0 else DOT for i, num in enumerate(data) for _ in range(int(num))]
-    v = blocks.copy()
+
     while DOT in blocks:
         if blocks[-1] == DOT:
             blocks.pop() # Just throw it away
         else:
-            index = blocks.index(DOT)
+            index = blocks.index(DOT) # Adds another loop each time through blocks.
             blocks[index] = blocks.pop()
     
-
     return [i * num for i, num in enumerate(blocks)]
 
 
+@timer
+def defrag_cleaner_and_faster(data):
+    ''' Time complexity of O(f*fs + k + cb) where k is free space not defragmented and cb are actual contiguous blocks.'''
+    blocks = [i // 2 if i % 2 == 0 else DOT for i, num in enumerate(data) for _ in range(int(num))]
+
+    i, j = 0, len(blocks) - 1
+    while i < len(blocks):
+        while i < j and blocks[j] == DOT:
+            j -= 1
+        if i < j and blocks[i] == DOT:
+            blocks[i], blocks[j] = blocks[j], blocks[i]
+        i += 1
+ 
+    return [i * num for i, num in enumerate(blocks) if num != DOT]
 
 
 def main(args, data):
     data = data.strip()
 
-    checksum = sum(defrag_original(data))
-    checksum = sum(defrag_clean(data))
+    checksum1 = sum(defrag_original(data))
+    checksum2 = sum(defrag_cleaner_and_faster(data))
+    checksum3 = sum(defrag_cleaner_but_slower(data))
+    assert checksum1 == checksum2 == checksum3
     
-    assertions(args, checksum, 1928, 6398252054886) 
+    assertions(args, checksum1, 1928, 6398252054886)
 
-    return checksum
+    return checksum1
     
 
 
 if __name__ == "__main__":
     args = arg_parse(__file__, 'input1.txt', main)
-    args = arg_parse(__file__, 'input3.txt', main)
+    args = arg_parse(__file__, 'input2.txt', main)
 
