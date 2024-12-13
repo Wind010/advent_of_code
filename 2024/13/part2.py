f6090ae2c6ec776469f9092d010ace4d6320c850
wind010 <wind010@users.noreply.github.com>
2024-12-12 23:37:37 -0800

    Day 13 of 2024 part2 with cheesy z3 solver.

--- 2024/13/part2.py
+++ 2024/13/part2.py
@@ -3,27 +3,14 @@ https://adventofcode.com/2024/day/13
 '''
 
 
-
-from collections import defaultdict
 import re
 from common.common import arg_parse, assertions, timer
 import math
 import z3
-
-def parse_input(lines):
-    a, b, prizes = [], [], []
-    for line in lines:
-        matches = re.findall(r'\d+', line)
-        if 'A' in line:
-            a.append(tuple(map(int, matches)))
-        elif 'B' in line:
-            b.append(tuple(map(int, matches)))
-        elif 'Prize' in line:
-            prizes.append(tuple(map(int, matches)))
-            
-    return a, b, prizes
+from part1 import parse_input
 
 
+CONVERSION_FACTOR = 10000000000000
 
 def find_fewest_tokens_brute_force(a, b, prizes):
     tokens = 0
@@ -32,7 +19,7 @@ def find_fewest_tokens_brute_force(a, b, prizes):
         a_x, a_y = a[i]
         b_x, b_y = b[i]
 
-        max_times = 100
+        max_times = CONVERSION_FACTOR * 2
         
         sol_x, sol_y = [], [] 
         for x in range(max_times):
@@ -66,6 +53,7 @@ def find_fewest_tokens_z3(a, b, prizes):
         
         for j in range(2):
             a_x, b_x, prize = a[i][j], b[i][j], prizes[i][j]
+            prize += CONVERSION_FACTOR
             solver.add(a_x * t_a + b_x * t_b == prize)
             
         if solver.check() == z3.sat:                
@@ -79,22 +67,22 @@ def main(args, data):
     
     a, b, prizes = parse_input(lines)
 
-    # 94x + 22x = 8400 & 34y + 67y = 5400
-    tokens1 = find_fewest_tokens_brute_force(a, b, prizes)
-    
-    # Could use https://github.com/Z3Prover/z3
+    # Could have tried binary search brute force?
+    # https://book.jorianwoltjer.com/cryptography/custom-ciphers/z3-solver
+    # https://www.youtube.com/watch?v=7WvXmoyfiTE
+    #tokens1 = find_fewest_tokens_brute_force(a, b, prizes)
     tokens2 = find_fewest_tokens_z3(a, b, prizes)
     
-    assert tokens1 == tokens2
+    #assert tokens1 == tokens2
     
-    assertions(args, tokens1, 480, 25629, 38839)
+    assertions(args, tokens2, 0, 107487112929999, 75200131617108)
 
-    return tokens1
+    return tokens2
     
 
 
 if __name__ == "__main__":
-    args = arg_parse(__file__, 'input1.txt', main)
+    args = arg_parse(__file__, 'input4.txt', main)
     args = arg_parse(__file__, 'input2.txt', main)
     args = arg_parse(__file__, 'input3.txt', main)
 
--- common/common.py
+++ common/common.py
@@ -5,6 +5,7 @@ import time
 from functools import wraps
 
 from typing import Callable
+from colorama import Fore, Back, Style
 
 # If class, then __init__.py would resolve.
 
@@ -32,11 +33,12 @@ def assertions(args, value, *expected_values):
     print(value)
     file_name = args.file_path.split('/')[-1]  # Extract the file name from the path
     for i, expected_value in enumerate(expected_values, start=1):
-        if f'input{i}.txt' == file_name:
-            assert value == expected_value, f"Expected {expected_value} for {file_name}, but got {value}! 💀"
+        if f'input{i}.txt' in file_name:
+            assert value == expected_value, f"💀 Expected {expected_value} for {file_name}, but got {Back.RED}{value}{Back.RESET}!"
+            print(f"✅ '{file_name}' yields {Back.GREEN}{value}{Back.RESET}")
             break
     else:
-        print(f"No expected value provided for {file_name} ⁉️")
+        print(f"⚠️ No expected value provided for {file_name} ⁉️")
 
 
 def timer(func):
