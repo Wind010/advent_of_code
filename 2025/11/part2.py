813e370cd195fe8e1e53013b5571b315537c5ad7
wind010 <wind010@users.noreply.github.com>
2025-12-10 22:51:59 -0800

    2025 Day 11 - Part 2 partial

--- 2025/11/part2.py
+++ 2025/11/part2.py
@@ -2,24 +2,40 @@
 https://adventofcode.com/2025/day11
 '''
 
+# We can filter for paths that include 'dac' AND 'fft'.
 
-
-import re
 from common.common import arg_parse, assertions, timer
+import networkx as ntwx
+
+@timer
+def find_all_relevant_paths(lines, source, target, nodes_to_include=['dac', 'fft']):
+    G = ntwx.DiGraph()
+    
+    for line in lines:
+        src, *dest = line.split()
+        for node in dest:
+            G.add_edge(src.strip(':'), node)
+    
+    return [path for path in ntwx.all_simple_paths(G, source, target)
+            if all(node in path for node in nodes_to_include)]
+
+# Takes too long.  Need to optimize with memoization possibly.
+
 
 
 def main(args, data):
     lines = data.strip().split('\n')
-    
-    total = 0
+    #x = find_all_relevant_paths(lines, 'svr', 'out')
+    total_paths = len(find_all_relevant_paths(lines, 'svr', 'out'))
 
-    assertions(args, total, 1, 1, 1)
-    return total
+
+    assertions(args, total_paths, 1, 1, 1, 2)
+    return total_paths
     
 
 
 if __name__ == "__main__":
-    args = arg_parse(__file__, 'input1.txt', main)
+    args = arg_parse(__file__, 'input4.txt', main)
     args = arg_parse(__file__, 'input2.txt', main)
     args = arg_parse(__file__, 'input3.txt', main)
 
