df918bd39bb436b3972390d050e8fccfdce3354f
wind010 <wind010@users.noreply.github.com>
2025-12-10 21:13:09 -0800

    2025 Day 10 - Part 2 fix

--- 2025/10/part2.py
+++ 2025/10/part2.py
@@ -2,12 +2,12 @@
 https://adventofcode.com/2025/day10
 '''
 
+# Use PuLP or SciPy linprog to solve as integer linear programming problem.
 
 from part1 import parse_input
 from common.common import arg_parse, assertions, timer
 from scipy.optimize import linprog
 
-# Too lazy, use PuLP or similar library to solve integer linear algebra programming problem.
 
 def find_min_presses_linear_alg(presses, joltages):
     presses, joltages = list(map(tuple, presses)), tuple(*joltages)
@@ -16,9 +16,7 @@ def find_min_presses_linear_alg(presses, joltages):
     eqs = [[i in p for p in presses] for i in range(len(joltages))]
     # min(cost * x) subject to eqs * x = joltages
     
-    res = linprog(costs, A_eq=eqs, b_eq=joltages, integrality=1).fun
-    return int(res) if res else 0
-
+    return linprog(costs, A_eq=eqs, b_eq=joltages, integrality=1).fun
 
 
 def main(args, data):
@@ -26,8 +24,9 @@ def main(args, data):
     #print(lights, presses, joltage)
 
     total = sum(find_min_presses_linear_alg(presses[i], joltages[i]) for i in range(len(lights)))
-
-    assertions(args, total, 33, 17575, 17848)
+    total = int(total) # If we cast to int within the loop we are off by 1, sometimes, due to rounding.
+    
+    assertions(args, total, 33, 17576, 17848)
     return total
     
 
